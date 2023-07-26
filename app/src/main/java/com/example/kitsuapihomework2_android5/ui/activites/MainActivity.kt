b1f27a0a12b3e9dc0e208789d867d8e3665f11a8
Zununova <zununiazk@gmail.com>
2023-07-26 17:13:58 +0300

    Connected viewBinding and viewPropertyDelegate

--- app/src/main/java/com/example/kitsuapihomework2_android5/ui/activites/MainActivity.kt
+++ app/src/main/java/com/example/kitsuapihomework2_android5/ui/activites/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kitsuapihomework2_android5
+package com.example.kitsuapihomework2_android5.ui.activites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kitsuapihomework2_android5.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
